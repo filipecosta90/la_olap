49f5afaf3b625021c41935450b4153ad3d4bb2cc
filipecosta90 <filipecosta.90@gmail.com>
2016-03-16 14:58:54 +0000

    added quark translation

--- src/quark_test.c
+++ src/quark_test.c
@@ -3,34 +3,35 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char* getfield(char* line, int num){
-const char* tok;
-  for (tok = strtok(line, ",");
-      tok && *tok;
-      tok = strtok(NULL, ",\n"))
-  {
-    if (!--num)
-      return tok;
+char* getfield(char* line, int num, char* return_string ){
+  return_string = strtok(line, ",");
+  for ( int pos = 1; pos <= num; pos++ ){
+    return_string = strtok(NULL, ",\n");
   }
-  return NULL;
+  return return_string;
 }
 
 int main( int argc, char* argv[]){
   GList* list = NULL;
-
   FILE* stream = fopen(argv[1], "r");
 
   char line[1024];
+
   int column = atoi (argv[2]); 
   while (fgets(line, 1024, stream))
   {
+    char *field = (char*) malloc( 128 * sizeof(char) );
     char* tmp = strdup(line);
-    list = g_list_append(list, getfield(tmp,column)); 
-    printf("Field 3 would be %s\n", getfield(tmp, column));
-    // NOTE strtok clobbers tmp
+    field = getfield(tmp, column, field);
+    GQuark* quark;
+    quark = g_quark_from_string (field);
+    quark = g_quark_from_string (field);
+    gchar* gnu_string;
+    gnu_string = g_quark_to_string(quark);
+    printf("Field %d would be: {%s}\tQUARK: {%d}\tRE-CONVERTED: {%s}\n", column, field, quark, gnu_string );
+    list = g_list_append(list, quark ); 
     free(tmp);
   }
-
-  printf("The first item is '%s'\n", g_list_first(list)->data);
+  printf("The first item is '%d' %s \n", g_list_first(list)->data, g_quark_to_string( g_list_first(list)->data ) );
   return 0;
 }
