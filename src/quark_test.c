75547fc4834381d3833dfc867a20decd9caabfc0
filipecosta90 <filipecosta.90@gmail.com>
2016-03-29 20:43:06 +0100

    corrected makefile

--- src/quark_test.c
+++ src/quark_test.c
@@ -5,7 +5,8 @@
 
 char* getfield(char* line, int num, char* return_string ){
   return_string = strtok(line, ",");
-  for ( int pos = 1; pos <= num; pos++ ){
+  int pos = 1;
+  for ( ; pos <= num; pos++ ){
     return_string = strtok(NULL, ",\n");
   }
   return return_string;
@@ -17,7 +18,8 @@ int main( int argc, char* argv[]){
 
   char line[1024];
 
-  int column = atoi (argv[2]); 
+  int column = atoi (argv[2]);
+  int line_num = 0;
   while (fgets(line, 1024, stream))
   {
     char *field = (char*) malloc( 128 * sizeof(char) );
@@ -28,9 +30,10 @@ int main( int argc, char* argv[]){
     quark = g_quark_from_string (field);
     gchar* gnu_string;
     gnu_string = g_quark_to_string(quark);
-    printf("Field %d would be: {%s}\tQUARK: {%d}\tRE-CONVERTED: {%s}\n", column, field, quark, gnu_string );
+    printf("%d - %d \t Field %d would be: {%s}\tQUARK: {%d}\tRE-CONVERTED: {%s}\n", line_num, quark, column, field, quark, gnu_string );
     list = g_list_append(list, quark ); 
     free(tmp);
+    line_num++;
   }
   printf("The first item is '%d' %s \n", g_list_first(list)->data, g_quark_to_string( g_list_first(list)->data ) );
   return 0;
