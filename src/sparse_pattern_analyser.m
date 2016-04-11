9da78e07d709965d653a4811bbe143fb5709e695
filipecosta90 <filipecosta.90@gmail.com>
2016-04-11 12:29:07 +0100

    added more articles concercing BIG Data

--- src/sparse_pattern_analyser.m
+++ src/sparse_pattern_analyser.m
@@ -1,10 +1,10 @@
 clear;
-load __sparsity_csv/1_15_lineitem.csv
-H = spconvert(X1_15_lineitem);
+load __sparsity_csv/1_9_lineitem.csv
+H = spconvert(X1_9_lineitem);
 
 hFig = figure(1);
-set(gcf,'PaperPositionMode','auto')
-set(hFig, 'Position', [0 0 960 960])
+%set(gcf,'PaperPositionMode','auto')
+%set(hFig, 'Position', [0 0 960 960])
 spy(H);
 [m,n] = size(H);
 non_zeros = nnz (H);
@@ -12,6 +12,6 @@ percentage = ( non_zeros / ( m * n ) ) * 100 ;
 set(gca,'YTickLabel',num2str(get(gca,'YTick')'));
 set(gca,'XTickLabel',num2str(get(gca,'XTick')'));
 
-str = sprintf('Sparsity pattern for Sparse Matrix based on GNU QUARKS, TPC-H 1, lineitem table, index row 1, data row 15, Non-Zero Percentage: %.5f %%',percentage);
+str = sprintf('Sparsity pattern for Sparse Matrix based on GNU QUARKS, TPC-H 1\n lineitem table, index row 1, data row 9, Non-Zero Percentage: %.5f %%,\n dimensions matrix (%d,%d)',percentage, m, n);
 title(str);
 
