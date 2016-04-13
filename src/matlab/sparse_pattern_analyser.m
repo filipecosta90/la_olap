9755e3113fe4d9f24ac46d07280e7fd4147cb282
filipecosta90 <filipecosta.90@gmail.com>
2016-04-13 12:28:27 +0100

    porting to windows for debug

--- src/matlab/sparse_pattern_analyser.m
+++ src/matlab/sparse_pattern_analyser.m
@@ -1,11 +1,11 @@
 load __sparsity_csv/1_2_lineitem.csv
 H = spconvert(X1_2_lineitem);
-load base64testing/2_base64_lineitem.csv
-B = spconvert(X2_base64_lineitem);
+load base64testing/15_base64_lineitem.csv
+B = spconvert(X15_base64_lineitem);
 
 hFig = figure(1);
-%set(gcf,'PaperPositionMode','auto')
-%set(hFig, 'Position', [0 0 960 960])
+set(gcf,'PaperPositionMode','auto')
+set(hFig, 'Position', [0 0 960 960])
 spy(H,'r');
 hold on;
 spy(B,'b');
@@ -24,6 +24,6 @@ set(gca,'YTickLabel',num2str(get(gca,'YTick')'));
 set(gca,'XTickLabel',num2str(get(gca,'XTick')'));
 
 
-str = sprintf('Sparsity pattern for Sparse Matrix based on GNU QUARKS vs Base64 Encoding,\nTPC-H 1 lineitem table, data row 2, Non-Zero Percentage: %.5f %% GNU QUARKS vs %.15f %% Base64 Encoding,\nmatrices dimensions: ( %d x %d ) GNU QUARKS vs ( %d x %d ) Base64 Encoding',percentage, percentageB, m, n, mB, nB);
+str = sprintf('Sparsity pattern for Sparse Matrix based on GNU QUARKS vs Base64 Encoding,\nTPC-H 1 lineitem table, projection row 15, Non-Zero Percentage: %.5f %% GNU QUARKS vs %.15f %% Base64 Encoding,\nmatrices dimensions: ( %d x %d ) GNU QUARKS vs ( %d x %d ) Base64 Encoding',percentage, percentageB, m, n, mB, nB);
 title(str);
 
