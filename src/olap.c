4860a8d95a14383e6fba97153bb5a3300e5fefca
filipecosta90 <filipecosta.90@gmail.com>
2016-04-14 19:14:49 +0100

    altered coo csr conversion. still with minor bugs

--- src/olap.c
+++ src/olap.c
@@ -2,7 +2,11 @@
 #include <glib.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include "mkl_spblas.h"
+#include "mkl_types.h"
 #include "mkl.h"
+
 
 //Cache-Lines size is (typically) 64 bytes
 #define MEM_LINE_SIZE 64
@@ -45,12 +49,11 @@ void check_errors(MKL_INT stat ){
 }
 
 int main( int argc, char* argv[]){
-
   MKL_INT current_values_size = ARRAY_SIZE;
 
   //define COO sparse-matrix M
   MKL_INT* aux_coo_rows;
-  aux_coo_rows = (int*) malloc (current_values_size * sizeof(int));
+  aux_coo_rows = (MKL_INT*) malloc (current_values_size * sizeof(MKL_INT));
 
   FILE* stream = fopen(argv[1], "r");
   char line[1024];
@@ -59,25 +62,26 @@ int main( int argc, char* argv[]){
   MKL_INT number_columns = -1 ;
   MKL_INT element_number = 1;
 
-  for( element_number = 1 ; (fgets(line, MAX_REG_SIZE, stream) ) ; ++element_number )
+  for( element_number = 0 ; (fgets(line, MAX_REG_SIZE, stream) ) ; ++element_number )
   {
     char *field = (char*) malloc( MAX_FIELD_SIZE * sizeof(char) );
     char* tmp_field = strdup(line);
     field = getfield(tmp_field, column, field);
-    unsigned int quark_field;
-    quark_field = g_quark_from_string (field);
+    MKL_INT quark_field;
 
+    quark_field = g_quark_from_string (field);
+    printf("%s %d\n", field, quark_field-1);
     /* if arrays are full double its size */
-    if ( element_number > current_values_size ){
+    if ( element_number >= current_values_size ){
       current_values_size *= GROWTH_FACTOR;
-      aux_coo_rows =  realloc(aux_coo_rows, (current_values_size) * GROWTH_FACTOR * sizeof(MKL_INT) );
+      aux_coo_rows = (MKL_INT*) realloc(aux_coo_rows, (current_values_size) * GROWTH_FACTOR * sizeof(MKL_INT) );
     }
 
     /* normal coo property */
-    aux_coo_rows[element_number-1]= (short) quark_field;
+    aux_coo_rows[element_number]=  quark_field -1 ;
 
-    if ( (short) quark_field > number_rows ) {
-      number_rows = ( short ) quark_field;
+    if (  quark_field > number_rows ) {
+      number_rows =  quark_field;
     }
     free(tmp_field);
   }
@@ -90,16 +94,17 @@ int main( int argc, char* argv[]){
   MKL_INT* coo_rows;
   MKL_INT* coo_columns;
 
-  coo_values = mkl_malloc ((NNZ * sizeof(float)), MEM_LINE_SIZE );
-  coo_rows =  mkl_malloc ((NNZ * sizeof(MKL_INT)), MEM_LINE_SIZE );
-  coo_columns =  mkl_malloc ((NNZ * sizeof(MKL_INT)), MEM_LINE_SIZE );
+  coo_values = (float*) mkl_malloc ((NNZ * sizeof(float)), MEM_LINE_SIZE );
+  coo_rows =  (MKL_INT*) mkl_malloc ((NNZ * sizeof(MKL_INT)), MEM_LINE_SIZE );
+  coo_columns =  (MKL_INT*) mkl_malloc ((NNZ * sizeof(MKL_INT)), MEM_LINE_SIZE );
 
   for (int pos = 0; pos < element_number; ++pos) {
     coo_values[pos] = 1.0;
     coo_columns[pos] = pos;
-    coo_rows[pos] = aux_coo_rows[pos]-1;
+    coo_rows[pos] = aux_coo_rows[pos];
+    printf("%f %d %d\n", coo_values[pos], coo_rows[pos], coo_columns[pos]);
   }
-  //free(aux_coo_rows);
+  mkl_free(aux_coo_rows);
 
   /////////////////////////////////
   //
@@ -111,23 +116,23 @@ int main( int argc, char* argv[]){
 
   MKL_INT job[6] = {
     1, // if job[0]=2, the matrix in the coordinate format is converted to the CSR
-       // format, and the column indices in CSR representation are sorted in the
-       // increasing order within each row.
+    // format, and the column indices in CSR representation are sorted in the
+    // increasing order within each row.
     0, // If job[1]=0, zero-based indexing for the matrix in CSR format is used;
     0, // If job[2]=0, zero-based indexing for the matrix in coordinate format is
-       //used;
+    //used;
     0, 
     NNZ,// job[4]=nzmax - maximum number of the non-zero elements allowed if
-        // job[0]=0.
+    // job[0]=0.
     0   // If job[5]=0, all arrays acsr, ja, ia are filled in for the output storage.
   };
 
   MKL_INT* JA;
   MKL_INT* IA;
 
-  csr_values = mkl_malloc ((element_number * sizeof(float)), MEM_LINE_SIZE );
-  JA = mkl_malloc (( element_number * sizeof(MKL_INT)), MEM_LINE_SIZE );
-  IA = mkl_malloc ((number_columns+1 * sizeof(MKL_INT)), MEM_LINE_SIZE );
+  csr_values = (float*) mkl_malloc ((element_number * sizeof(float)), MEM_LINE_SIZE );
+  JA = (MKL_INT*) mkl_malloc (( element_number * sizeof(MKL_INT)), MEM_LINE_SIZE );
+  IA = (MKL_INT*) mkl_malloc ((number_columns+1 * sizeof(MKL_INT)), MEM_LINE_SIZE );
 
   printf("going to convert matrix N x M = %d x %d\nNNZ: %d JOB4: %d\n", number_rows, number_columns, NNZ, job[4]);
   MKL_INT info=-1;
@@ -146,6 +151,11 @@ int main( int argc, char* argv[]){
   }
   printf("\n");
 
+  mkl_free(coo_values);
+  mkl_free(coo_rows);
+  mkl_free(coo_columns);
+
+
   /////////////////////////////////
   //
   //   CONVERT FROM CSR TO BSR
@@ -163,9 +173,9 @@ int main( int argc, char* argv[]){
   float* bsr_values = NULL;
   MKL_INT* JAB;
   MKL_INT* IAB;
-  bsr_values = mkl_malloc ((NNZ * sizeof(float)), MEM_LINE_SIZE );
-  JAB = mkl_malloc ((NNZ * sizeof(MKL_INT)), MEM_LINE_SIZE );
-  IAB = mkl_malloc (((number_columns+1) * sizeof(MKL_INT)), MEM_LINE_SIZE );
+  bsr_values = (float*) mkl_malloc ((NNZ * sizeof(float)), MEM_LINE_SIZE );
+  JAB = (MKL_INT*) mkl_malloc ((NNZ * sizeof(MKL_INT)), MEM_LINE_SIZE );
+  IAB = (MKL_INT*) mkl_malloc (((number_columns+1) * sizeof(MKL_INT)), MEM_LINE_SIZE );
 
   MKL_INT info_bsr=-1;
   MKL_INT mblk = 1;
