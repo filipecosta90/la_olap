8e54258c7cfef9aac6ab206ffb4b883a335fda69
filipecosta90 <filipecosta.90@gmail.com>
2016-05-14 21:19:01 +0100

    added test results for dataset 1GB

--- src/compile_seq.sh
+++ src/compile_seq.sh
@@ -3,6 +3,7 @@
 # Preparing environment for Parallel Studio XE 2016 and enabling ifort 16
 echo "Loading Parallel Studio XE 2016 Compilers ..."
 module purge
+cd 
 cd /share/apps/intel/parallel_studio_xe_2016.0.047/compilers_and_libraries_2016/linux/bin
 source compilervars_global.sh intel64
 cd /share/apps/intel/parallel_studio_xe_2016.0.047/compilers_and_libraries_2016/linux/mkl/bin
