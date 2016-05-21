08d7ea50697d5569add63c748cca27dba0d065e9
SergioCaldas <sergiocaldas.91@gmail.com>
2016-05-21 19:23:39 +0100

    Implemented queriy-1 jno optimization

--- src/querie1_search_v1.c
+++ src/querie1_search_v1.c
@@ -1,5 +1,5 @@
 /* ---------------------------------------------------------------------------
- **    Filename: querie1_search.c
+ **    Filename: querie1_search_v1.c
  **
  **     License: This file is part of OLAP PROJECT.
  **
@@ -44,187 +44,199 @@
 double global_time_start, global_time_stop, total_time;
 
 void writeResults ( ) {
-  total_time = global_time_stop - global_time_start;
-  FILE* stream = fopen("timing/timings_no_vec_32.dat", "a+");
-  fprintf(stream, "32,%f\n", total_time);
-  fclose(stream);
+	total_time = global_time_stop - global_time_start;
+	FILE* stream = fopen("timing/timings_no_vec_32.dat", "a+");
+	fprintf(stream, "32,%f\n", total_time);
+	fclose(stream);
 }
 
 int main( int argc, char* argv[]){
 
-  //define CSR sparse-matrix M
+	//define CSR sparse-matrix M
 
-  __declspec(align(MEM_LINE_SIZE))  float* returnFlag_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* returnFlag_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* returnFlag_IA;
-  MKL_INT returnFlag_rows;
-  MKL_INT returnFlag_columns;
-  MKL_INT returnFlag_nnz;
+	__declspec(align(MEM_LINE_SIZE))  float* returnFlag_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* returnFlag_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* returnFlag_IA;
+	MKL_INT returnFlag_rows;
+	MKL_INT returnFlag_columns;
+	MKL_INT returnFlag_nnz;
 
-  __declspec(align(MEM_LINE_SIZE))  float* lineStatus_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* lineStatus_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* lineStatus_IA;
-  MKL_INT lineStatus_rows;
-  MKL_INT lineStatus_columns;
-  MKL_INT lineStatus_nnz;
+	__declspec(align(MEM_LINE_SIZE))  float* lineStatus_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* lineStatus_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* lineStatus_IA;
+	MKL_INT lineStatus_rows;
+	MKL_INT lineStatus_columns;
+	MKL_INT lineStatus_nnz;
 
-  __declspec(align(MEM_LINE_SIZE))  float* quantity_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* quantity_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* quantity_IA;
-  MKL_INT quantity_rows;
-  MKL_INT quantity_columns;
-  MKL_INT quantity_nnz;
-  sparse_matrix_t  quantity_matrix;
+	__declspec(align(MEM_LINE_SIZE))  float* quantity_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* quantity_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* quantity_IA;
+	MKL_INT quantity_rows;
+	MKL_INT quantity_columns;
+	MKL_INT quantity_nnz;
+	sparse_matrix_t  quantity_matrix;
 
-  __declspec(align(MEM_LINE_SIZE))  float* shipdate_gt_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* shipdate_gt_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* shipdate_gt_IA;
-  MKL_INT shipdate_gt_rows;
-  MKL_INT shipdate_gt_columns;
-  MKL_INT shipdate_gt_nnz;
-  sparse_matrix_t  shipdate_gt_matrix;
+	__declspec(align(MEM_LINE_SIZE))  float* shipdate_gt_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* shipdate_gt_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* shipdate_gt_IA;
+	MKL_INT shipdate_gt_rows;
+	MKL_INT shipdate_gt_columns;
+	MKL_INT shipdate_gt_nnz;
+	sparse_matrix_t  shipdate_gt_matrix;
 
-  __declspec(align(MEM_LINE_SIZE))  float* shipdate_lt_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* shipdate_lt_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* shipdate_lt_IA;
-  MKL_INT shipdate_lt_rows;
-  MKL_INT shipdate_lt_columns;
-  MKL_INT shipdate_lt_nnz;
-  sparse_matrix_t  shipdate_lt_matrix;
+	__declspec(align(MEM_LINE_SIZE))  float* shipdate_lt_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* shipdate_lt_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* shipdate_lt_IA;
+	MKL_INT shipdate_lt_rows;
+	MKL_INT shipdate_lt_columns;
+	MKL_INT shipdate_lt_nnz;
+	sparse_matrix_t  shipdate_lt_matrix;
 
-  __declspec(align(MEM_LINE_SIZE))  float* selection_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* selection_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* selection_IA;
-  MKL_INT selection_rows;
-  MKL_INT selection_columns;
-  MKL_INT selection_nnz;
-  sparse_matrix_t  selection_matrix;
+	__declspec(align(MEM_LINE_SIZE))  float* selection_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* selection_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* selection_IA;
+	MKL_INT selection_rows;
+	MKL_INT selection_columns;
+	MKL_INT selection_nnz;
+	sparse_matrix_t  selection_matrix;
 
-  __declspec(align(MEM_LINE_SIZE))  float* projection_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* projection_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* projection_IA;
-  MKL_INT projection_rows;
-  MKL_INT projection_columns;
-  MKL_INT projection_nnz;
-  sparse_matrix_t  projection_matrix;
+	__declspec(align(MEM_LINE_SIZE))  float* projection1_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* projection1_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* projection1_IA;
+	MKL_INT projection1_rows;
+	MKL_INT projection1_columns;
+	MKL_INT projection1_nnz;
+	sparse_matrix_t  projection1_matrix;
 
-  __declspec(align(MEM_LINE_SIZE)) float* aggregation_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* aggregation_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* aggregation_IA;
-  MKL_INT aggregation_rows;
-  MKL_INT aggregation_columns;
-  MKL_INT aggregation_nnz;
+	__declspec(align(MEM_LINE_SIZE))  float* projection2_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* projection2_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* projection2_IA;
+	MKL_INT projection2_rows;
+	MKL_INT projection2_columns;
+	MKL_INT projection2_nnz;
+	sparse_matrix_t  projection2_matrix;
 
-  __declspec(align(MEM_LINE_SIZE)) float* intermediate_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* intermediate_JA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* intermediate_IA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT intermediate_rows;
-  MKL_INT intermediate_columns;
-  MKL_INT intermediate_nnz;
-  sparse_matrix_t  intermediate_matrix;
+	__declspec(align(MEM_LINE_SIZE)) float* aggregation_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* aggregation_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* aggregation_IA;
+	MKL_INT aggregation_rows;
+	MKL_INT aggregation_columns;
+	MKL_INT aggregation_nnz;
 
-  __declspec(align(MEM_LINE_SIZE)) float* final_csr_values = NULL;
-  __declspec(align(MEM_LINE_SIZE)) MKL_INT* final_JA;
-  __declspec(align(MEM_LINE_SIZE)) MKL_INT* final_IA;
-  __declspec(align(MEM_LINE_SIZE))  MKL_INT* final_IA_END;
-  MKL_INT final_rows;
-  MKL_INT final_columns;
-  MKL_INT final_nnz;
-  sparse_matrix_t  final_matrix;
+	__declspec(align(MEM_LINE_SIZE)) float* intermediate_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* intermediate_JA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* intermediate_IA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT intermediate_rows;
+	MKL_INT intermediate_columns;
+	MKL_INT intermediate_nnz;
+	sparse_matrix_t  intermediate_matrix;
 
-  __declspec(align(MEM_LINE_SIZE))  float* bang_vector;
-  __declspec(align(MEM_LINE_SIZE))  float* aggregation_vector;
+	__declspec(align(MEM_LINE_SIZE)) float* final_csr_values = NULL;
+	__declspec(align(MEM_LINE_SIZE)) MKL_INT* final_JA;
+	__declspec(align(MEM_LINE_SIZE)) MKL_INT* final_IA;
+	__declspec(align(MEM_LINE_SIZE))  MKL_INT* final_IA_END;
+	MKL_INT final_rows;
+	MKL_INT final_columns;
+	MKL_INT final_nnz;
+	sparse_matrix_t  final_matrix;
 
-  //conversion status from csr arrays into mkl sparse_matrix_t 
-  sparse_status_t status_to_csr;
+	__declspec(align(MEM_LINE_SIZE))  float* bang_vector;
+	__declspec(align(MEM_LINE_SIZE))  float* aggregation_vector;
 
-  //read return flag
-  read_from_mx("__quark_mx_32/return_flag_32.mx", &returnFlag_csr_values, &returnFlag_JA, &returnFlag_IA, &returnFlag_nnz, &returnFlag_rows, &returnFlag_columns);
+	//conversion status from csr arrays into mkl sparse_matrix_t 
+	sparse_status_t status_to_csr;
 
-  //read line status
-  read_from_mx("__quark_mx_32/line_status_32.mx", &lineStatus_csr_values, &lineStatus_JA, &lineStatus_IA, &lineStatus_nnz, &lineStatus_rows, &lineStatus_columns);
+	//read return flag
+	read_from_mx("__quark_mx_1/return_flag_1.mx", &returnFlag_csr_values, &returnFlag_JA, &returnFlag_IA, &returnFlag_nnz, &returnFlag_rows, &returnFlag_columns);
 
-  //read quantity
-  read_from_mx("__quark_mx_32/quantity_32.mx", &quantity_csr_values, &quantity_JA, &quantity_IA, &quantity_nnz, &quantity_rows, &quantity_columns);
+	//read line status
+	read_from_mx("__quark_mx_1/line_status_1.mx", &lineStatus_csr_values, &lineStatus_JA, &lineStatus_IA, &lineStatus_nnz, &lineStatus_rows, &lineStatus_columns);
 
-  //        convert via sparseBLAS API to Handle containing internal data for 
-  //        subsequent Inspector-executor Sparse BLAS operations.
-  status_to_csr = mkl_sparse_s_create_csr ( &quantity_matrix , SPARSE_INDEX_BASE_ZERO, 
-      quantity_rows, quantity_columns, quantity_IA, quantity_IA+1, quantity_JA, quantity_csr_values );
+	//read quantity
+	read_from_mx("__quark_mx_1/quantity_1.mx", &quantity_csr_values, &quantity_JA, &quantity_IA, &quantity_nnz, &quantity_rows, &quantity_columns);
 
-  //read shipdate gt
-  read_from_mx("__quark_mx_32/shipdate_gt_32.mx", &shipdate_gt_csr_values, &shipdate_gt_JA, &shipdate_gt_IA, &shipdate_gt_nnz, &shipdate_gt_rows, &shipdate_gt_columns);
+	//        convert via sparseBLAS API to Handle containing internal data for 
+	//        subsequent Inspector-executor Sparse BLAS operations.
+	status_to_csr = mkl_sparse_s_create_csr ( &quantity_matrix , SPARSE_INDEX_BASE_ZERO, 
+			quantity_rows, quantity_columns, quantity_IA, quantity_IA+1, quantity_JA, quantity_csr_values );
 
-  //        convert via sparseBLAS API to Handle containing internal data for
-  //        subsequent Inspector-executor Sparse BLAS operations.
-  status_to_csr = mkl_sparse_s_create_csr ( &shipdate_gt_matrix , SPARSE_INDEX_BASE_ZERO,
-      shipdate_gt_rows, shipdate_gt_columns, shipdate_gt_IA, shipdate_gt_IA+1, shipdate_gt_JA, shipdate_gt_csr_values );
+	//read shipdate gt
+	read_from_mx("__quark_mx_1/shipdate_gt_1.mx", &shipdate_gt_csr_values, &shipdate_gt_JA, &shipdate_gt_IA, &shipdate_gt_nnz, &shipdate_gt_rows, &shipdate_gt_columns);
 
-  //read shipdate lt
-  read_from_mx("__quark_mx_32/shipdate_lt_32.mx", &shipdate_lt_csr_values, &shipdate_lt_JA, &shipdate_lt_IA, &shipdate_lt_nnz, &shipdate_lt_rows, &shipdate_lt_columns);
+	//        convert via sparseBLAS API to Handle containing internal data for
+	//        subsequent Inspector-executor Sparse BLAS operations.
+	status_to_csr = mkl_sparse_s_create_csr ( &shipdate_gt_matrix , SPARSE_INDEX_BASE_ZERO,
+			shipdate_gt_rows, shipdate_gt_columns, shipdate_gt_IA, shipdate_gt_IA+1, shipdate_gt_JA, shipdate_gt_csr_values );
 
-  //        convert via sparseBLAS API to Handle containing internal data for
-  //        subsequent Inspector-executor Sparse BLAS operations.
-  status_to_csr = mkl_sparse_s_create_csr ( &shipdate_lt_matrix , SPARSE_INDEX_BASE_ZERO,
-      shipdate_lt_rows, shipdate_lt_columns, shipdate_lt_IA, shipdate_lt_IA+1, shipdate_lt_JA, shipdate_lt_csr_values );
-  // compute selection = shipdate_gt * shipdate_lt 
-  sparse_status_t selection_result;
+	//read shipdate lt
+	read_from_mx("__quark_mx_1/shipdate_lt_1.mx", &shipdate_lt_csr_values, &shipdate_lt_JA, &shipdate_lt_IA, &shipdate_lt_nnz, &shipdate_lt_rows, &shipdate_lt_columns);
 
-  ////////////////////////
-  // START TIME MEASUREMENT
-  ////////////////////////
-  GET_TIME(global_time_start);
+	//        convert via sparseBLAS API to Handle containing internal data for
+	//        subsequent Inspector-executor Sparse BLAS operations.
+	status_to_csr = mkl_sparse_s_create_csr ( &shipdate_lt_matrix , SPARSE_INDEX_BASE_ZERO,
+			shipdate_lt_rows, shipdate_lt_columns, shipdate_lt_IA, shipdate_lt_IA+1, shipdate_lt_JA, shipdate_lt_csr_values );
+	// compute selection = shipdate_gt * shipdate_lt 
+	sparse_status_t selection_result;
 
-  selection_result = mkl_sparse_spmm ( SPARSE_OPERATION_NON_TRANSPOSE,
-      shipdate_gt_matrix,
-      shipdate_lt_matrix,
-      &selection_matrix);
-  // compute projection = returnFlag krao lineStatus
-  csr_krao(
-      returnFlag_csr_values, returnFlag_JA, returnFlag_IA, 
-      returnFlag_nnz, returnFlag_rows, returnFlag_columns,
-      lineStatus_csr_values, lineStatus_JA, lineStatus_IA ,
-      lineStatus_nnz, lineStatus_rows, lineStatus_columns,
-      &projection_csr_values, &projection_JA, &projection_IA, 
-      &projection_nnz, &projection_rows, &projection_columns  
-      );
+	////////////////////////
+	// START TIME MEASUREMENT
+	////////////////////////
+	GET_TIME(global_time_start);
 
-  status_to_csr = mkl_sparse_s_create_csr ( &projection_matrix , SPARSE_INDEX_BASE_ZERO, projection_rows, projection_columns, projection_IA, projection_IA+1, projection_JA, projection_csr_values );
+	selection_result = mkl_sparse_spmm ( SPARSE_OPERATION_NON_TRANSPOSE,
+			shipdate_gt_matrix,
+			shipdate_lt_matrix,
+			&selection_matrix);
 
-  // compute aggregation = quantity * bang
+	// compute projection1 = returnFlag krao quantity
+	csr_krao(
+			returnFlag_csr_values, returnFlag_JA, returnFlag_IA, 
+			returnFlag_nnz, returnFlag_rows, returnFlag_columns,
+			quantity_csr_values, quantity_JA, quantity_IA ,
+			quantity_nnz, quantity_rows,quantity_columns,
+			&projection_csr_values, &projection_JA, &projection_IA, 
+			&projection_nnz, &projection_rows, &projection_columns  
+		);
 
-  bang_vector = (float*) mkl_malloc ((quantity_columns * sizeof(float)), MEM_LINE_SIZE );
-  aggregation_vector = (float*) mkl_malloc ((quantity_columns * sizeof(float)), MEM_LINE_SIZE );
+	status_to_csr = mkl_sparse_s_create_csr ( &projection_matrix , SPARSE_INDEX_BASE_ZERO, projection_rows, projection_columns, projection_IA, projection_IA+1, projection_JA, projection_csr_values );
 
-  sparse_status_t aggregation_result;
-  struct matrix_descr descrA;
-  descrA.type = SPARSE_MATRIX_TYPE_GENERAL;
 
-  aggregation_result = mkl_sparse_s_mv ( SPARSE_OPERATION_NON_TRANSPOSE, 1.0, quantity_matrix , descrA, bang_vector, 1.0,  aggregation_vector);
+	// compute projection2 = returnFlag krao quantity
+	csr_krao(
+			lineStatus_csr_values, lineStatus_JA, lineStatus_IA, 
+			lineStatus_nnz, lineStatus_rows, lineStatus_columns,
+			selection_csr_values, selection_JA, selection_IA ,
+			selection_nnz, selection_rows, selection_columns,
+			&projectioni2_csr_values, &projection2_JA, &projection2_IA, 
+			&projection2_nnz, &projection2_rows, &projection2_columns  
+		);
 
-  // compute intermediate_result = projection * selection
-  sparse_status_t intermediate_result;
+	//compute aggregation = projection2 * bang
 
-  intermediate_result = mkl_sparse_spmm ( SPARSE_OPERATION_NON_TRANSPOSE, 
-      projection_matrix,
-      selection_matrix, 
-      &intermediate_matrix);
+	bang_vector = (float*) mkl_malloc ((quantity_columns * sizeof(float)), MEM_LINE_SIZE );
+	aggregation_vector = (float*) mkl_malloc ((quantity_columns * sizeof(float)), MEM_LINE_SIZE );
 
-  // compute final_result = intermediate_result * aggregation
-  sparse_status_t final_result;
+	sparse_status_t aggregation_result;
+	struct matrix_descr descrA;
+	descrA.type = SPARSE_MATRIX_TYPE_GENERAL;
 
-  final_result = mkl_sparse_spmm ( SPARSE_OPERATION_NON_TRANSPOSE, 
-      intermediate_matrix,
-      quantity_matrix,
-      &final_matrix);
+	aggregation_result = mkl_sparse_s_mv ( SPARSE_OPERATION_NON_TRANSPOSE, 1.0, projection2_matrix , descrA, bang_vector, 1.0,  aggregation_vector);
 
-  ////////////////////////
-  // STOP TIME MEASUREMENT
-  ////////////////////////
-  GET_TIME(global_time_stop);
-  writeResults( );
+	// compute final_result = projection1 * aggregation
+	sparse_status_t final_result;
 
-  return 0;
+	final_result = mkl_sparse_spmm ( SPARSE_OPERATION_NON_TRANSPOSE, 
+			projection1_matrix,
+			projection2_matrix,
+			&final_matrix);
+
+	////////////////////////
+	// STOP TIME MEASUREMENT
+	////////////////////////
+	GET_TIME(global_time_stop);
+	writeResults( );
+
+	return 0;
 
 }
 
