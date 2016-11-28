13b408307f1155103f82e18c4da93de5e87bb917
filipecosta90 <filipecosta.90@gmail.com>
2016-11-28 16:54:06 +0000

    changed the approach on the parsing method. now for each col element we have its position based on the file start and its size

--- src/parser/olap_driver.cu
+++ src/parser/olap_driver.cu
@@ -20,54 +20,59 @@ struct is_newline_break{
   __host__ __device__                                                           
     bool operator()(const char x)                                               
     {                                                                           
-      return x == 10;                                                           
+      return x == '\n';                                                           
     }                                                                           
 };
 
 struct parse_functor
 {
   const char *source;
-  char **dest;
+  int **dest_col_start;
+  int **dest_col_size;
   const unsigned int *ind;
-  const unsigned int *cnt;
-  const char *separator;
-  const int *src_ind;
+  const unsigned int *num_cols_to_parse;
+  const char *field_separator;
+  const int *src_newline_ind;
   const unsigned int *dest_len;
 
   parse_functor(
-      const char* _source, char** _dest, 
+      const char* _source, int** _dest_col_start, int** _dest_col_size, 
       const unsigned int* _ind, const unsigned int* _cnt, 
       const char* _separator,
       const int* _src_ind, const unsigned int* _dest_len
       ):
-    source(_source), dest(_dest), ind(_ind), cnt(_cnt),  separator(_separator), src_ind(_src_ind), dest_len(_dest_len) {}
+    source(_source), dest_col_start(_dest_col_start), dest_col_size(_dest_col_size), ind(_ind), num_cols_to_parse(_cnt),  field_separator(_separator), src_newline_ind(_src_ind), dest_len(_dest_len) {}
 
   template <typename IndexType>
     __host__ __device__
     void operator()(const IndexType & i) {
-      unsigned int curr_cnt = 0, dest_curr = 0, j = 0, t, pos;
-      pos = src_ind[i]+1;
+      unsigned int current_column = 0, total_parsed_cols = 0, inline_pos = 0, in_col_nonzeros, line_start;
+      line_start = src_newline_ind[i]+1;
 
-      while(dest_curr < *cnt) {
-        if(ind[dest_curr] == curr_cnt) { //process
-          t = 0;
-          while(source[pos+j] != *separator) {
-            if(source[pos+j] != 0) {
-              dest[dest_curr][dest_len[dest_curr]*i+t] = source[pos+j];
-              t++;
+      while(total_parsed_cols < *num_cols_to_parse){
+        // if its the column we want to parse
+        if(ind[total_parsed_cols] == current_column) { //process
+          dest_col_start[total_parsed_cols][i]=line_start+inline_pos;
+          in_col_nonzeros = 0;
+          while(source[line_start+inline_pos] != *field_separator){
+            if(source[line_start+inline_pos] != 0) {
+              in_col_nonzeros++;
             };
-            j++;
+            inline_pos++;
           };
-          j++;
-          dest_curr++;
+          //save the size of the column
+          dest_col_size[total_parsed_cols][i]=in_col_nonzeros;
+          inline_pos++;
+          total_parsed_cols++;
         }
-        else {
-          while(source[pos+j] != *separator) {
-            j++;
+        // ignore the current column
+        else{
+          while(source[line_start+inline_pos] != *field_separator) {
+            inline_pos++;
           };
-          j++;
+          inline_pos++;
         };
-        curr_cnt++;
+        current_column++;
       }
     }
 };
@@ -98,7 +103,6 @@ namespace OLAP{
     parse_helper( stream );
     return;
   }
-
 
   void OLAP_Driver::parse_helper( std::istream &stream ){
     delete(scanner);
@@ -153,7 +157,6 @@ namespace OLAP{
     }                                                                             
 
     thrust::copy(mapped_file, mapped_file+file_size, dev.begin());                                     
-    std::cout << "going to count" << std::endl;
     int line_count = std::count(dev.begin(), dev.end(), '\n');                        
     std::cout << "There are " << line_count << " total lines in a file with size " << file_size << std::endl;    
 
@@ -165,9 +168,7 @@ namespace OLAP{
         dev.begin(), dev_newline_pos.begin()+1, 
         is_newline_break()
         ); 
-    for ( int pos = 0; pos < dev_newline_pos.size(); pos++ ){
-      std::cout << dev_newline_pos[pos] << std::endl;
-    }
+
     // field position based on column number
     thrust::device_vector<unsigned int> field_index(1);
     field_index[0]=col_number;
@@ -176,11 +177,15 @@ namespace OLAP{
     thrust::device_vector<char> field_separator(1);
     field_separator[0] = '|';
 
-    thrust::device_vector<char> dev_res1(line_count*max_col_size); 
-    thrust::fill(dev_res1.begin(), dev_res1.end(), 0); 
+    thrust::device_vector<int> dev_col_start1(line_count); 
+    thrust::device_vector<int> dev_col_size1(line_count); 
+    thrust::fill(dev_col_start1.begin(), dev_col_start1.end(), 0); 
+    thrust::fill(dev_col_size1.begin(), dev_col_size1.end(), 0); 
 
-    thrust::device_vector<char*> dest(1);
-    dest[0] = thrust::raw_pointer_cast(dev_res1.data());
+    thrust::device_vector<int*> dest_col_start(1);
+    thrust::device_vector<int*> dest_col_size(1);
+    dest_col_start[0] = thrust::raw_pointer_cast(dev_col_start1.data());
+    dest_col_size[0] = thrust::raw_pointer_cast(dev_col_size1.data());
 
     //fields max lengths 
     thrust::device_vector<unsigned int> dest_len(1); 
@@ -194,7 +199,8 @@ namespace OLAP{
 
     parse_functor ff(
         (const char*) thrust::raw_pointer_cast(dev.data()),
-        (char**) thrust::raw_pointer_cast(dest.data()), 
+        (int**) thrust::raw_pointer_cast(dest_col_start.data()), 
+        (int**) thrust::raw_pointer_cast(dest_col_size.data()), 
         thrust::raw_pointer_cast(field_index.data()),
         thrust::raw_pointer_cast(ind_cnt.data()), 
         thrust::raw_pointer_cast(field_separator.data()), 
@@ -202,13 +208,12 @@ namespace OLAP{
         thrust::raw_pointer_cast(dest_len.data())
         );
     thrust::for_each(begin, begin + line_count, ff);
-    
-    for (int pos = 0; pos < (line_count * max_col_size ); pos ++  ){
-      std::cout << dev_res1[pos];
-      if( (pos % (max_col_size+1) )== 0 ){
-        std::cout << std::endl;
-      }
+/*
+    for (int pos = 0; pos < line_count ; pos ++  ){
+      std::string col ( &(mapped_file[dev_col_start1[pos]]), dev_col_size1[pos] );
+      std::cout << dev_col_start1[pos] << " " << dev_col_size1[pos] << " : " << col<<  std::endl;
     }
+*/
     std::cout << "leaving load matrix " << std::endl;
   }
 
